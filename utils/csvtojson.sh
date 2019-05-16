9d35bb86d82e17e7cb10fe30a2016307d1568d71
thomasgarai <thomasgarai@zefa.cloud>
2019-05-16 22:49:35 +1000

    Age of Discovery II

---
+++ utils/csvtojson.sh
@@ -0,0 +1,45 @@
+input=$1
+ 
+[ -z $1 ] && echo "No CSV input file specified" && exit 1
+[ ! -e $input ] && echo "Unable to locate $1" && exit 1
+ 
+read first_line < $input
+a=0
+headings=`echo $first_line | awk -F, {'print NF'}`
+lines=`cat $input | wc -l`
+while [ $a -lt $headings ]
+do
+  head_array[$a]=$(echo $first_line | awk -v x=$(($a + 1)) -F"," '{print $x}')
+  a=$(($a+1))
+done
+ 
+c=0
+echo "{"
+while [ $c -lt $lines ]
+do
+  read each_line
+  if [ $c -ne 0 ]; then
+    d=0
+    echo -n "{"
+    while [ $d -lt $headings ]
+    do
+      each_element=$(echo $each_line | awk -v y=$(($d + 1)) -F"," '{print $y}')
+      if [[ $each_element == [a-zA-Z]* ]]; then
+        each_element="\"${each_element}\""                     
+      fi
+      if [ $d -ne $(($headings-1)) ]; then
+        echo -n "\"${head_array[$d]}\":$each_element,"
+      else
+        echo -n "\"${head_array[$d]}\":$each_element"
+      fi
+      d=$(($d+1))
+    done
+    if [ $c -eq $(($lines-1)) ]; then
+      echo "}"
+    else
+      echo "},"
+    fi
+  fi
+c=$(($c+1))
+done < $input
+echo "}"
---
+++ utils/startws.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#
+cd /home/tg/games/SA/AgeOfDiscoveryII
+php -S localhost:8765
